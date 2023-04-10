1dc8c53f1e2a9942238c61439cd66026005bd980
rigolinux <rigoberto7567@gmail.com>
2023-04-10 15:09:09 -0600

    implement entities header and details recipe from products

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
+import {
+  RecipeHeader,
+  RecipeHeaderSchema,
+  RecipeDetails,
+  RecipeDetailsSchema,
+} from './entities/';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: RecipeHeader.name, schema: RecipeHeaderSchema },
+      { name: RecipeDetails.name, schema: RecipeDetailsSchema },
+    ]),
+  ],
   controllers: [InventoryController],
-  providers: [InventoryService]
+  providers: [InventoryService],
 })
 export class InventoryModule {}

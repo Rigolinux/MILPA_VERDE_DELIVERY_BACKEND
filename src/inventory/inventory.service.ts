51b3d62cb4b48236b377a05faabe726d3f05d309
rigolinux <rigoberto7567@gmail.com>
2023-04-11 11:06:55 -0600

    inject the models

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { CreateInventoryDto } from './dto/create-inventory.dto';
-import { UpdateInventoryDto } from './dto/update-inventory.dto';
+import { CreateInventoryDto, CreateRecipeDto } from './dto/';
+
+import { Model, Types } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { RecipeHeader, RecipeDetails } from './entities/';
 
 @Injectable()
 export class InventoryService {
+  constructor(
+    @InjectModel(RecipeHeader.name)
+    private readonly recipeHeaderModel: Model<RecipeHeader>,
+    @InjectModel(RecipeDetails.name)
+    private readonly recipeDetailsModel: Model<RecipeDetails>,
+  ) {}
+
+  createRecipe(createRecipeDto: CreateRecipeDto) {
+    const recipeHeader = new this.recipeHeaderModel(createRecipeDto);
+    const recipeDetails = new this.recipeDetailsModel(createRecipeDto.details);
+    recipeHeader.details = recipeDetails;
+    recipeHeader.save();
+    return recipeHeader;
+  }
+
   create(createInventoryDto: CreateInventoryDto) {
     return 'This action adds a new inventory';
   }

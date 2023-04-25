62b546cb2b77ca03198064ed43e49f03a9e2857a
rigolinux <rigoberto7567@gmail.com>
2023-04-25 11:11:34 -0600

    Inject the models

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSalesHeaderDto, UpdateSaleDto } from './dto';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { SalesHeader, SalesDetails } from './entities';
+
 @Injectable()
 export class SalesService {
+  constructor(
+    @InjectModel(SalesHeader.name)
+    private readonly SalesHeaderModel: Model<SalesHeader>,
+    @InjectModel(SalesDetails.name)
+    private readonly SalesDetailsModel: Model<SalesDetails>,
+  ) {}
+
   create(createSaleDto: CreateSalesHeaderDto) {
     return 'This action adds a new sale';
   }
